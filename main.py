ab093d685ac394e15a95bde49c8bcee14707b84e
owenowenisme <mses010108@gmail.com>
2023-08-31 19:23:50 +0100

    add jwt validation

--- main.py
+++ main.py
@@ -9,10 +9,17 @@ from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
 from typing import Annotated, Union
 import logging
-from google.auth import jwt
+from google.auth import jwt, exceptions
+from google.oauth2 import id_token
+from google.auth.transport import requests
 
-app = FastAPI()
 
+
+
+app = FastAPI(docs_url=None, redoc_url=None)
+clientId = "761442466271-4e3pel8pnajc5lcv4c4psd1n83mb06os.apps.googleusercontent.com"
+logger = logging.getLogger("uvicorn.access")
+handler = logging.handlers.RotatingFileHandler("./api.log",mode="a",maxBytes = 10*1024*1024, backupCount = 3)
 
 logging.basicConfig(level=logging.INFO)
 
@@ -33,6 +40,8 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/main/{course_id:path}")
 def list_all(course_id:str ,db: cursor = Depends(get_db)):
+    handler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(message)s -"))
+    logger.addHandler(handler)
     query = f"SELECT * FROM pastexam.files WHERE course_id = '{course_id}' ORDER BY year DESC;"
     db.execute(query)
     r = [dict((db.description[i][0], value) for i, value in enumerate(row)) for row in db.fetchall()]
@@ -44,6 +53,8 @@ def list_all(course_id:str ,db: cursor = Depends(get_db)):
 @app.get("/courselist")
 async def get_courselist(
                    db: cursor = Depends(get_db),key: str = 'none'):
+    handler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(message)s -"))
+    logger.addHandler(handler)
     if key == 'none':
         query = "SELECT * FROM pastexam.courses;"
     else :
@@ -57,15 +68,37 @@ async def get_courselist(
 
 @app.get("/files/")
 async def fetchfile(request: Request,course_id: int =0, file_name:str=''):
+    
     path=f"./static/{course_id}/{file_name}"    
 
     oauth_jwt=request.headers.get('token')
+    
     if(oauth_jwt == None):
         return JSONResponse(
             status_code=status.HTTP_400_BAD_REQUEST,
             content = {"message":"Login first!"}
         )
-    if(jwt.decode(oauth_jwt, verify=False).get('hd') != 'gs.ncku.edu.tw'):
+    try: 
+        userinfo=id_token.verify_oauth2_token(oauth_jwt, requests.Request(), clientId)
+    except exceptions.InvalidValue as e:
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content = {"message":"Token Expired! Please Relogin!"}
+        )
+    except Exception as e:
+        logging.info(e)
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content = {"message":"Unvalid Login! Please Relogin!"}
+        )
+
+
+    
+    forlog= f"{userinfo.get('email')} {userinfo.get('given_name')}"
+    handler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(message)s -"+forlog))
+    logger.addHandler(handler)
+    forlog=""
+    if(userinfo.get('hd') != 'gs.ncku.edu.tw'):
         return JSONResponse(
             status_code=status.HTTP_400_BAD_REQUEST,
             content = {"message":"Please use NCKU email to login!"}
@@ -79,9 +112,32 @@ async def fetchfile(request: Request,course_id: int =0, file_name:str=''):
 @app.post("/uploadfile/")
 async def upload_file( request: Request, file:UploadFile ,year: int = 0, examtype : str = '', teacher : str = '', course_id:int=0 ,token: str=''):
     oauth_jwt=request.headers.get('token')
+    
     if(oauth_jwt == None):
         return  {"status":"error","message":"Login first!"}
-    userinfo=jwt.decode(oauth_jwt, verify=False)
+
+
+    try: 
+        userinfo=id_token.verify_oauth2_token(oauth_jwt, requests.Request(), clientId)
+    except exceptions.InvalidValue as e:
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content = {"message":"Token Expired! Please Relogin!"}
+        )
+    except Exception as e:
+        logging.info(e)
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content = {"message":"Unvalid Login! Please Relogin!"}
+        )
+
+    if(course_id > 48):
+        return  {"status":"error","message":"Course do not exist!"}
+
+    forlog= f"{userinfo.get('email')} {userinfo.get('given_name')}"
+    handler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(message)s -"+forlog))
+    logger.addHandler(handler)
+
     if(userinfo.get('hd') != 'gs.ncku.edu.tw'):
         return  {"status":"error","message":"Please use NCKU email to login!"}
     if course_id==0 or examtype == '' or teacher == '' or year == 0:
